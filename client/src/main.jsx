baf067239ed0588e61804b7cb7b5d02d47b2a641
Pranav Gupta <kpranav.in@gmail.com>
2023-07-29 14:13:23 +0530

    Updated code

---
+++ client/src/main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0Provider
+     domain="dev-03ifqltxbr6nn0hn.us.auth0.com"
+     clientId="RXlGXkr49Ev5MHpvAC6vKkZ4bVn11iwl"
+     authorizationParams={{
+      redirect_uri: "https://full-stack-real-estate-youtube-lyart.vercel.app"
+     }}
+     audience="http://localhost:8000"
+     scope="openid profile email"
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
